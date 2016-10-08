eb06f07fe65798b9890d2b392e34d5fe1fa14348
donghuaxiao <x_donghua@163.com>
2016-10-08 16:48:46 +0800

    对DefaultPacketFilter 增加 打印包信息功能

--- src/com/ericsson/filter/DefaultPacketFilter.java
+++ src/com/ericsson/filter/DefaultPacketFilter.java
@@ -1,13 +1,56 @@
 package com.ericsson.filter;
 
+import java.nio.ByteBuffer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.jnetpcap.packet.PcapPacket;
-import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.protocol.tcpip.Tcp;
+
+import com.ericsson.parser.CMPPParser;
+import com.ericsson.protocol.CMPP;
+import com.ericsson.protocol.CMPPHeader;
+import com.ericsson.protocol.CMPPSubmit;
+import com.ericsson.util.Utils;
 
 public class DefaultPacketFilter implements PacketFilter {
-
-    Ip4 ip = new Ip4();
+	
+	private DateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	
+	Tcp tcp = new Tcp();
 
     public boolean isMatch(PcapPacket packet) {
-        return true;
+    	
+    	if ( packet.hasHeader(tcp)) {
+    		byte[] data = tcp.getPayload();
+    		if (data.length > 0) {
+    			ByteBuffer buffer = ByteBuffer.wrap(data);
+                CMPPHeader cmppheader = CMPPParser.parserHeader(buffer);
+                if (cmppheader.getCmdtype() == CMPP.CMPP_SUBMIT) {
+                	CMPPSubmit submit = new CMPPSubmit();
+                    submit.setHeader(cmppheader);
+                    submit.setBody(buffer);                    
+                    printSubmitMessage(submit);
+                    return true;
+                }
+
+            }
+    	}
+        return false;
+    }
+    
+    /**
+     * 打印 CMPP Submit 消息
+     * 格式： 序列号  时间   手机号  消息内容
+     * @param packet
+     */
+    public void printSubmitMessage( CMPPSubmit packet) {
+    	String msisdns = Utils.join(packet.getDestTerminalId(), ", ");
+    	System.out.printf("%s\t%s\t%s\t%s\n", 
+    			packet.getHeader().getSequenceNumber(), 
+    			dataFormat.format(new Date()), 
+    			msisdns, 
+    			packet.getMsgContent().trim());
     }
 }
